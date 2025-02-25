ddadfc0d13d22042987b71811f10dc6183a8e10f
Angel <aal51282@uga.edu>
2025-02-25 03:58:39 -0500

    Created second activity.

--- app/src/main/java/com/example/project3/MasterDetailActivity.java
+++ app/src/main/java/com/example/project3/MasterDetailActivity.java
@@ -1,24 +1,56 @@
 package com.example.project3;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MasterDetailActivity extends AppCompatActivity {
+public class MasterDetailActivity extends AppCompatActivity implements MajorListFragment.OnMajorSelectedListener {
+
+    // Determines if we’re running in two-pane mode.
+    private boolean twoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_master_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+
+        // If the detail container exists, we’re in landscape (two-pane) mode.
+        if (findViewById(R.id.detail_container) != null) {
+            twoPane = true;
+        }
+
+        // Load the MajorListFragment into the proper container.
+        if (savedInstanceState == null) {
+            if (twoPane) {
+                // In landscape, load list fragment into list_container.
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.list_container, new MajorListFragment())
+                        .commit();
+            } else {
+                // In portrait, load list fragment into single container.
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MajorListFragment())
+                        .commit();
+            } // if
+        } // if
+
+    } // onCreate
+
+    // This callback is triggered when a major is selected in the list fragment.
+    @Override
+    public void onMajorSelected(String major) {
+        DetailFragment detailFragment = DetailFragment.newInstance(major);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (twoPane) {
+            // In landscape, show detail fragment in the detail container.
+            transaction.replace(R.id.detail_container, detailFragment);
+        } else {
+            // In portrait, replace the entire container and add the transaction to the back stack.
+            transaction.replace(R.id.fragment_container, detailFragment)
+                    .addToBackStack(null);
+        } // if
+        transaction.commit();
+    } // onMajorSelected
+
+} // MasterDetailActivity
