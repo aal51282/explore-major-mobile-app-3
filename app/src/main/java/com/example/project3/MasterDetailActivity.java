19361e8390dbda4afebd743acd5889c118512cfb
Angel <aal51282@uga.edu>
2025-02-25 19:56:58 -0500

    Fixed the project.

--- app/src/main/java/com/example/project3/MasterDetailActivity.java
+++ app/src/main/java/com/example/project3/MasterDetailActivity.java
@@ -22,38 +22,63 @@ public class MasterDetailActivity extends AppCompatActivity implements MajorList
         // Determine if we're in two-pane (landscape) mode by checking for the detail_container.
         twoPane = (findViewById(R.id.detail_container) != null);
 
-        // Restore the selected major if available.
-        if (savedInstanceState != null) {
+        // Only set up fragments if this is the first creation
+        if (savedInstanceState == null) {
+            setupInitialFragments();
+        } else {
+            // Restore the selected major
             selectedMajor = savedInstanceState.getString(STATE_SELECTED_MAJOR);
-            // Remove any fragment that was automatically restored in the portrait container.
-            Fragment restoredFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-            if (restoredFragment != null) {
-                getSupportFragmentManager().beginTransaction().remove(restoredFragment).commit();
-            } // if
-        } // if
+
+            // Handle fragment restoration after configuration change
+            handleFragmentRestoration();
+        }
+    }
+
+    private void setupInitialFragments() {
+        if (twoPane) {
+            // In landscape, add MajorListFragment to list_container
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.list_container, new MajorListFragment())
+                    .commit();
+        } else {
+            // In portrait, add MajorListFragment to fragment_container
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new MajorListFragment())
+                    .commit();
+        }
+    }
+
+    private void handleFragmentRestoration() {
+        // We need to handle fragment restoration manually
+        getSupportFragmentManager().executePendingTransactions();
 
         if (twoPane) {
-            // In landscape, add the MajorListFragment into list_container if not already added.
-            if (getSupportFragmentManager().findFragmentById(R.id.list_container) == null) {
+            // We're in landscape mode now
+            // Make sure list fragment is in the correct container
+            MajorListFragment listFragment = (MajorListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.fragment_container);
+
+            if (listFragment != null) {
+                // Move the list fragment to the correct container
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.list_container, new MajorListFragment())
+                        .remove(listFragment)
+                        .add(R.id.list_container, new MajorListFragment())
                         .commit();
-            } // if
-            // If a major was selected before rotation, display its detail.
+                getSupportFragmentManager().executePendingTransactions();
+            }
+
+            // Add the detail fragment if we have a selected major
             if (selectedMajor != null) {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.detail_container, DetailFragment.newInstance(selectedMajor))
                         .commit();
-            } // if
+            }
         } else {
-            // In portrait, add MajorListFragment into fragment_container if it's not already there.
-            if (getSupportFragmentManager().findFragmentById(R.id.fragment_container) == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, new MajorListFragment())
-                        .commit();
-            } // if
-        } // if
-    } // onCreate
+            // We're in portrait mode now
+            // Any necessary adjustments to fragments in portrait mode would go here
+            // Usually no special handling needed for portrait restoration
+        }
+    }
 
     /**
      * Callback when a major is selected.
@@ -81,4 +106,42 @@ public class MasterDetailActivity extends AppCompatActivity implements MajorList
         super.onSaveInstanceState(outState);
         outState.putString(STATE_SELECTED_MAJOR, selectedMajor);
     } // onSaveInstanceState
+
+    @Override
+    public void onConfigurationChanged(android.content.res.Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Save current fragments state and selected major
+        String currentMajor = selectedMajor;
+
+        // Reload the layout
+        setContentView(R.layout.activity_master_detail);
+
+        // Check for two-pane mode
+        boolean newTwoPane = (findViewById(R.id.detail_container) != null);
+
+        // Handle fragment transitions
+        if (newTwoPane && !twoPane) {
+            // Portrait to landscape transition
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.list_container, new MajorListFragment())
+                    .commit();
+
+            if (currentMajor != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.detail_container, DetailFragment.newInstance(currentMajor))
+                        .commit();
+            }
+        } else if (!newTwoPane && twoPane) {
+            // Landscape to portrait transition
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new MajorListFragment())
+                    .commit();
+        }
+
+        // Update the twoPane state
+        twoPane = newTwoPane;
+        selectedMajor = currentMajor;
+    }
+
 } // MasterDetailActivity
